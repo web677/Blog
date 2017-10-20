625f3bf3ffca3a9dd6a506582f36781d6d646db7
“eshengeshu” <eshengeshu.blog>
2017-10-20 17:19:13 +0800

    更新app.js && 终于跑通了

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const Koa = require("koa")
 const fs = require("fs")
-const index = require("./dist/index.html")
-
 const app = new Koa()
 
+app.use(async (ctx, next) => {
+    	let reqPath = ctx.path
+	if(reqPath == "/" || reqPath == "/login" || reqPath == "/regist"){
+        	 ctx.response.type = 'html';
+         	 ctx.response.body = fs.createReadStream('./dist/index.html');
+    	}
+	
+    	if(/\/static/.test(reqPath) && /.css$/.test(reqPath)){
+		ctx.response.type = "text/css"
+		ctx.response.body = fs.createReadStream("./dist" + reqPath)
+	}
 
-app.use(async ctx => {
-    if(ctx.path == "/" || ctx.path == "/login" || ctx.path == "/regist"){
-        ctx.status = 200
-        ctx.body = index
-        return
-    }
-    ctx.body = "404"
+	if(/\/static/.test(reqPath) && /.js$/.test(reqPath)){
+                ctx.response.type = "application/javascript"
+                ctx.response.body = fs.createReadStream("./dist" + reqPath)
+        }
+
+	if(/\/static/.test(reqPath) && /.png$/.test(reqPath)){
+                ctx.response.type = "image/png"
+                ctx.response.body = fs.createReadStream("./dist" + reqPath)
+        }
 })
+
 
 app.listen(3000)
 
